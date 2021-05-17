1a70b2bc1c833f3c9ee00c6bb7a3a7e8251b13bc
Farhan Hasan <hasanfarhan33@gmail.com>
2021-05-17 19:21:01 +0300

    Added homepage with buttons

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+export default function App() {  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.background}>
+      <TouchableOpacity onPress={() => {/*Do something here*/}}>
+        <View style={styles.productsButtonContainer}> 
+          <Text style={styles.productsButtonText}>
+            Products
+          </Text>
+        </View>
+
+      </TouchableOpacity>
+{/* Categories Button is here */}
+      <TouchableOpacity onPress={() => {/*Do something here*/}}>
+        <View style={styles.categoriesButtonContainer}> 
+          <Text style={styles.categoriesButtonText}>
+            Categories
+          </Text>
+        </View>
+      </TouchableOpacity>
+
+      {/* Orders Button is here */}
+      <TouchableOpacity onPress={() => {/*Do something here*/}}>
+        <View style={styles.ordersButtonContainer}> 
+          <Text style={styles.ordersButtonText}>
+            Orders
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    backgroundColor: '#F7B2AD',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',  
+    paddingVertical: 250,  
+    paddingHorizontal: 20, 
+    flex: 1, 
+    
+    
   },
+
+  // Products Button
+  productsButtonContainer: 
+  {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 22,
+    elevation: 5,
+  }, 
+  productsButtonText: 
+  {
+    color: "white", 
+    fontSize: 40, 
+    textAlign: "center", 
+    fontFamily: 'Roboto',
+    fontStyle: 'italic', 
+  }, 
+
+  // Categories Button
+  categoriesButtonContainer: 
+  {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 22, 
+    elevation: 5,
+  }, 
+
+  categoriesButtonText:
+  {
+    color: "white", 
+    fontSize: 40, 
+    textAlign: "center",
+    fontFamily: 'Roboto',
+    fontStyle: 'italic',  
+  },
+
+  // Orders Button
+  ordersButtonContainer:
+  {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 20,
+    paddingHorizontal: 22,
+    elevation: 5, 
+
+  }, 
+  ordersButtonText:
+  {
+    color: "white", 
+    fontSize: 40, 
+    textAlign: "center",
+    fontFamily: 'Roboto',
+    fontStyle: 'italic', 
+  }
 });
