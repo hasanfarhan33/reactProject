f7ab34c0c3d8a66cd983c7fd77b2421aa3c75435
Farhan Hasan <hasanfarhan33@gmail.com>
2021-05-23 16:43:12 +0300

    Added and Decorated About Screen

---
+++ About.js
@@ -0,0 +1,65 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState, Component } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
+import { NativeRouter, Switch, Route } from "react-router-native";
+
+export default ({ history }) => (
+  <View style ={styles.background}>
+    <Text style = {styles.about}>About</Text>
+    <Text style = {styles.info}>This app was developed by Farhan Hasan (1721647) and Ege Oz (1730762)</Text>
+    <TouchableOpacity onPress={() => {history.push("/")}}>
+        <View style = {styles.backButtonContainer}>
+            <Text style = {styles.back}>
+                Back to Homepage
+            </Text>
+        </View>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F7B2AD",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+
+  about:{
+      textAlign: "center", 
+      fontSize: 46,
+      fontWeight: "bold",
+      fontStyle: "italic", 
+      fontFamily: "Roboto",
+      color: "black",    
+  }, 
+
+  info: {
+      textAlign: "center", 
+      fontSize: 20, 
+      fontWeight: "bold", 
+      color: "black", 
+      fontStyle: "italic", 
+  },
+
+  backButtonContainer: {
+    backgroundColor: "#333232",
+    borderRadius: 30,
+    paddingVertical: 15,
+    marginHorizontal: 40, 
+    elevation: 5,
+  },
+
+  back: {
+    color: "white",
+    fontSize: 25,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontStyle: "italic",
+
+  }
+
+});
---
+++ Home.js
@@ -0,0 +1,144 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState, Component } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { NativeRouter, Switch, Route } from "react-router-native";
+
+export default ({ history }) => (
+    <View style={styles.background}>
+        <Text style={styles.paragraph}>BAU Market</Text>
+
+        <TouchableOpacity
+          onPress={() => {
+            /*Do something here*/
+          }}
+        >
+          <View style={styles.productsButtonContainer}>
+            <Text style={styles.productsButtonText}>Products</Text>
+          </View>
+        </TouchableOpacity>
+        {/* Categories Button is here */}
+        <TouchableOpacity
+          onPress={() => {
+            /*Do something here*/
+          }}
+        >
+          <View style={styles.categoriesButtonContainer}>
+            <Text style={styles.categoriesButtonText}>Categories</Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Orders Button is here */}
+        <TouchableOpacity
+          onPress={() => {
+            /*Do something here*/
+          }}
+        >
+          <View style={styles.ordersButtonContainer}>
+            <Text style={styles.ordersButtonText}>Orders</Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* About Button is here */}
+        <TouchableOpacity
+          onPress={() => {
+            history.push("/about")
+          }}
+        >
+          <View style={styles.aboutButtonContainer}>
+            <Text style={styles.aboutButtonText}>About</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+
+);
+
+// Designing stuff here
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F7B2AD",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+
+  // Title
+  paragraph: {
+    marginTop: -80,
+    fontSize: 46,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "black",
+    fontStyle: "italic",
+  },
+
+  // Products Button
+  productsButtonContainer: {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 15,
+    paddingHorizontal: 22,
+    elevation: 5,
+  },
+  productsButtonText: {
+    color: "white",
+    fontSize: 40,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+
+  // Categories Button
+  categoriesButtonContainer: {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 15,
+    paddingHorizontal: 22,
+    elevation: 5,
+  },
+
+  categoriesButtonText: {
+    color: "white",
+    fontSize: 40,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+
+  // Orders Button
+  ordersButtonContainer: {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 15,
+    paddingHorizontal: 22,
+    elevation: 5,
+  },
+  ordersButtonText: {
+    color: "white",
+    fontSize: 40,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+
+  // About Button
+  aboutButtonContainer: {
+    backgroundColor: "#333232",
+    borderRadius: 50,
+    paddingVertical: 15,
+    paddingHorizontal: 22,
+    elevation: 5,
+  },
+  aboutButtonText: {
+    color: "white",
+    fontSize: 40,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+});
